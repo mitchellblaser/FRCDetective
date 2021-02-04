12e247e9c61ad7e38c3a91ff0dc09a56e88219a4
Dhiluka Mapa <dhiluka.mapa@hotmail.com>
2021-02-04 21:28:39 +1100

    Initialised Mobile Client Project

---
+++ Client/FRC Detective/FRC Detective/FRC Detective/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FRC_Detective.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FRC_Detective.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
