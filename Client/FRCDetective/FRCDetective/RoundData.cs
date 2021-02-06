b997dbd2fff381e55ede574186cc3d24797ca265
Dhiluka Mapa <dhiluka.mapa@hotmail.com>
2021-02-06 17:57:41 +1100

    Added Local File Saving And Loading

---
+++ Client/FRCDetective/FRCDetective/RoundData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRCDetective
+{
+    class RoundData
+    {
+        public bool InitLine { get; set; }
+        public int AutoHighGoal { get; set; }
+        public int AutoLowGoal { get; set; }
+    }
+}
