f64cd895e343cbaed6f876d6beff84bb83277932
Mitch Blaser <mitchell.1080media@gmail.com>
2021-03-09 22:13:11 +1100

    Migrated from Flask to Production WSGI Server

--- Server/webgui/migrations/versions/9b44c1691178_.py
+++ Server/webgui/migrations/versions/9b44c1691178_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 1ee9e17f3c7c
+Revision ID: 9b44c1691178
 Revises: 
-Create Date: 2021-03-09 21:54:33.138807
+Create Date: 2021-03-09 22:10:54.576019
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1ee9e17f3c7c'
+revision = '9b44c1691178'
 down_revision = None
 branch_labels = None
 depends_on = None
--- Server/webgui/webgui.py
+++ Server/webgui/webgui.py
@@ -1,3 +1,5 @@
 from app import app
+from waitress import serve
 
-app.run(host="0.0.0.0", port=8080)
+#app.run(host="0.0.0.0", port=8080)
+serve(app, host='0.0.0.0', port=8080)
