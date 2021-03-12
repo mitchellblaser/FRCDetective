b8e95359e1074c3e80b14d52fdbb4c20b0fad4bb
Dhiluka Mapa <dhiluka.mapa@hotmail.com>
2021-03-13 00:12:41 +1100

    Initial saving implemented, not working

--- Client/FRCDetective/FRCDetective/NewGameEntryPage.xaml.cs
+++ Client/FRCDetective/FRCDetective/NewGameEntryPage.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
-using Xamarin.Essentials;
 
 using Newtonsoft.Json;
 using PCLStorage;
@@ -167,9 +166,108 @@ namespace FRCDetective
         void TechFoulsDecrement(object sender, EventArgs e) { StepperControl(LabelTechFouls, false, 1, false); }
 
         /* Saving */
+        public bool IsNumeric(string input)
+        {
+            int test;
+            return int.TryParse(input, out test);
+        }
         async void Save(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(TeamEntry.Text))
+            {
+                await DisplayAlert("Data Error", "Team Number Cannot Be Empty", "OK");
+                return;
+            }
+            else if (!IsNumeric(TeamEntry.Text))
+            {
+                await DisplayAlert("Data Error", "Team Number Must Be Numeric", "OK");
+                return;
+            }
+            /*else if (string.IsNullOrEmpty(RoundEntry.Text))
+            {
+                await DisplayAlert("Data Error", "Round Number Cannot Be Empty", "OK");
+                return;
+            }
+            else if (!IsNumeric(RoundEntry.Text))
+            {
+                await DisplayAlert("Data Error", "Round Number Must Be Numeric", "OK");
+                return;
+            }*/
+            else
+            {
+                // Init Data
+                RoundData round = new RoundData();
+                round.Team = Convert.ToInt32(TeamEntry.Text);
+                round.Round = Convert.ToInt32(RoundSelect.Text.Substring(RoundSelect.Text.IndexOf('Q') + 1));
+                round.Alliance = Convert.ToInt32(BtnToggleRedAlliance.BackgroundColor == Color.DarkSlateBlue);
+                round.Division = 0;
+                //round.Type = Convert.ToInt32(chkFinal.IsChecked);
+                round.Type = 0;                 // Change when round entry is added
+                round.Timestamp = DateTime.Now;
 
+                // Auto Data
+                round.InitLine = ChkInitLine.IsChecked;
+                round.AutoHighGoal = Convert.ToInt32(LabelAutoHighGoal.Text);
+                round.AutoLowGoal = Convert.ToInt32(LabelAutoLowGoal.Text);
+
+                // Teleop Data
+                round.TeleopHighGoal = Convert.ToInt32(LabelTeleHighGoal.Text);
+                round.TeleopLowGoal = Convert.ToInt32(LabelTeleLowGoal.Text);
+                round.ColourwheelRotation = (BtnTeleColorWheelRotation.BackgroundColor == Color.DarkSlateBlue);
+                round.ColourwheelPosition = (BtnTeleColorWheelPosition.BackgroundColor == Color.DarkSlateBlue);
+                if (TelePark.BackgroundColor == Color.DarkSlateBlue)
+                {
+                    round.Climb = 1;
+                }
+                else if (TeleClimb.BackgroundColor == Color.DarkSlateBlue)
+                {
+                    round.Climb = 2;
+                }
+                else
+                {
+                    round.Climb = 0;
+                }
+                round.Level = (TeleSwitchLevel.BackgroundColor == Color.DarkSlateBlue);
+
+                // Fouls
+                round.Foul = Convert.ToInt32(LabelFouls.Text);
+                round.TechFoul = Convert.ToInt32(LabelTechFouls.Text);
+
+
+
+
+                string DisplayName = "";
+                DisplayName += "Round ";
+                DisplayName += round.Round.ToString();
+                if (round.Alliance == 1) { DisplayName += " Red Alliance "; } else { DisplayName += " Blue Alliance "; }
+                DisplayName += "Team ";
+                DisplayName += round.Team.ToString();
+                round.DisplayName = DisplayName;
+
+
+                string ID = "";
+                ID += round.Division.ToString();
+                ID += "-";
+                ID += round.Type;
+                ID += "-";
+                ID += round.Round.ToString("D3");
+                ID += "-";
+                ID += round.Team.ToString("D5");
+
+                round.ID = ID;
+
+                string FileName = ID + ".json";
+                round.Filename = FileName;
+
+                string json = JsonConvert.SerializeObject(round);
+
+                IFolder rootFolder = FileSystem.Current.LocalStorage;
+                IFolder folder = await rootFolder.CreateFolderAsync("RoundData", CreationCollisionOption.OpenIfExists);
+                IFile file = await folder.CreateFileAsync(FileName, CreationCollisionOption.ReplaceExisting);
+                await file.WriteAllTextAsync(json);
+
+                await DisplayAlert("Message", "Done", "OK");
+            }
         }
     }
 }
