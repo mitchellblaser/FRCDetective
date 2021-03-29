071934a12cacf81db478cbaa96cf704210a2a3cc
Dhiluka Mapa <dhiluka.mapa@hotmail.com>
2021-03-29 19:38:28 +1100

    Moved FileViewPage to show round numbers rather than teams

---
+++ Client/FRCDetective/FRCDetective/RoundViewPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FRCDetective
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RoundViewPage : ContentPage
+    {
+        public RoundViewPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Refresh()
+        {
+
+
+        }
+        public async void OnDelete(object sender, EventArgs e)
+        {
+
+        }
+
+        async void ItemSelected(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
