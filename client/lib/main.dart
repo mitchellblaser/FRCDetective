3d7da30c55d88ab85b15e4c181c302617d3182c3
Mitch Blaser <mitchell.1080media@gmail.com>
2022-03-07 14:22:48 +1100

    Rebased code. File R/W now also working.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,39 +1,35 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:io';
 import 'dart:typed_data';
-import 'package:path_provider/path_provider.dart';
+
+import 'config.dart';
+import 'filehandler.dart';
+import 'customcolor.dart';
+
+import 'widgets/main/teaminformation.dart';
+import 'widgets/main/serversync.dart';
+import 'widgets/main/livestream.dart';
+import 'widgets/main/teamrankings.dart';
 
 //TODO: This does not currently support web applications because of the incompatible WebSocket. Create wrapper?
 
-const String applicationName = "FRCDetective";
+Icon serverState = const Icon(Icons.hourglass_bottom);
 
-Icon ServerState = Icon(Icons.hourglass_bottom);
-
-int serverPollIntervalMS = 10000;
-
-void doServerUpdate_initial() async {
-  final directory = await getApplicationDocumentsDirectory();
-  final AppFilePath = directory.path;
-
-  try {
-    final List<String> contents = await File(AppFilePath + "/server.txt").readAsLines();
-    print("CONTENTS0 $contents[0]");
-    print("CONTENTS1 $contents[1]");
-  } 
-  on FileSystemException catch (e) {
-    return;
-  }
+void doServerUpdateInitial() async {
+  final List<String> f = await readFile("server.txt");
+  _SERVER_ADDRESS_ = f[0];
+  _SERVER_PORT_ = f[1];
 
   doServerUpdate();
+  return;
 }
 
 void doServerUpdate() async {
     try {
       var s = await Socket.connect(_SERVER_ADDRESS_, int.parse(_SERVER_PORT_)).timeout(const Duration(seconds: 10)); //TODO: Error checking here in case server is not available
+      serverState = const Icon(Icons.link);
       s.write('{"request": "PUT_TEAM", "data": {"teamnumber": "5584"}}');
       s.listen(
         (Uint8List data) {
@@ -54,256 +50,43 @@ void doServerUpdate() async {
         }
       );
     }
-    on TimeoutException catch (e) {
+    on TimeoutException {
+      serverState = const Icon(Icons.link_off);
       return;
     }
-    on FormatException catch (e) {
-      print("FormatException: $e");
-      ServerState = Icon(Icons.warning);
+    on FormatException {
+      serverState = const Icon(Icons.warning);
+      return;
+    }
+    on SocketException {
+      serverState = const Icon(Icons.warning);
       return;
     }
 }
 
-double _boxHeight = 0;
-String _logoPath = "assets/images/logo.png";
-
 String _SERVER_ADDRESS_ = "";
 String _SERVER_PORT_ = "";
+
+String __SERVER_ADDRESS_ = "";
+String __SERVER_PORT_ = "";
+
+// double _boxHeight = 0;
+String _logoPath = "assets/images/logo.png";
 
 void main() {
   // Platform-specific code...
   if (!kIsWeb) {
     if (Platform.isWindows) {
-      _boxHeight = 14;
+      boxHeight = 14;
       _logoPath = "assets/images/logo-small.png";
     }
   }
   // Run GUI
   runApp(const DetectiveApp());
   // Start Periodic Functions
-  Timer.periodic(Duration(milliseconds: serverPollIntervalMS), (Timer t) => { doServerUpdate() });
-  doServerUpdate_initial();
+  Timer.periodic(const Duration(milliseconds: serverPollIntervalMS), (Timer t) => { doServerUpdate() });
+  doServerUpdateInitial();
 }
-
-TextStyle headerStyle = const TextStyle(fontSize: 48, fontFamily: 'LeagueSpartan');
-TextStyle bodyStyle = const TextStyle(fontSize: 20, fontFamily: 'Roboto', color: Colors.white);
-TextStyle bodySmallStyle = const TextStyle(fontSize: 16, fontFamily: 'Roboto');
-TextStyle bodyXSmallStyle = const TextStyle(fontSize: 14, fontFamily: 'Roboto');
-TextStyle bodyItalSmallStyle = const TextStyle(fontSize: 14, fontFamily: 'Roboto', fontStyle: FontStyle.italic, color: Colors.white);
-
-const _customColorValue = 0xFF000A59;
-const MaterialColor customColor = MaterialColor(
-  _customColorValue,
-  <int, Color>{
-    50: Color(0xFF000A59),
-    100: Color(0xFF000A59),
-    200: Color(0xFF000A59),
-    300: Color(0xFF000A59),
-    400: Color(0xFF000A59),
-    500: Color(0xFF000A59),
-    600: Color(0xFF000A59),
-    700: Color(0xFF000A59),
-    800: Color(0xFF000A59),
-    900: Color(0xFF000A59),
-  }
-);
-
-class TeamInformationWidget extends StatefulWidget {
-  const TeamInformationWidget({Key? key}) : super (key: key);
-  
-  @override
-  _TeamInformationWidgetState createState() => _TeamInformationWidgetState();
-}
-
-class _TeamInformationWidgetState extends State<TeamInformationWidget> {
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      Timer.periodic(const Duration(milliseconds: 100), (Timer t) => setState((){}));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(30),
-                splashColor: customColor.withAlpha(50),
-                // onTap: () {
-
-                // },
-                child: SizedBox(
-                  width: 370,
-                  height: 170 + _boxHeight,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(padding: const EdgeInsets.only(top: 16, left: 22), child: Align(alignment: Alignment.centerLeft, child: Text("Your Team", style: bodyStyle))),
-                      Container(padding: const EdgeInsets.only(top: 2, left: 20),child: Align(alignment: Alignment.centerLeft, child: Text("5584", style: headerStyle))),
-                      Container(padding: const EdgeInsets.only(top: 6, left: 20),child: Align(alignment: Alignment.centerLeft, child: Text("Chance of Victory: 58.2%", style: bodyItalSmallStyle))),
-                      Container(padding: const EdgeInsets.only(top: 0, left: 20), child: Align(alignment: Alignment.centerLeft,
-                        child: RichText(text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(text: "Qualifier 36 ", style: bodyStyle),
-                            TextSpan(text: "at 11:17am today.", style: bodyItalSmallStyle)
-                          ]
-                        ))
-                      )),
-                      
-                      Container(padding: const EdgeInsets.only(top: 4, left: 20), child: Align(alignment: Alignment.centerLeft,
-                        child: RichText(text: TextSpan(
-                          style: bodySmallStyle,
-                          children: const <TextSpan>[
-                            TextSpan(text: "5584 ", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)),
-                            TextSpan(text: "1234 5678 ", style: TextStyle(color: Colors.blue)),
-                            TextSpan(text: "9123 4567 8910", style: TextStyle(color: Colors.red))
-                          ]
-                        ))
-                      ))
-
-                    ],
-                  ),
-                ),
-              ),
-          );
-  }
-}
-
-class ServerSyncWidget extends StatefulWidget {
-  const ServerSyncWidget({Key? key}) : super (key: key);
-
-  @override
-  _ServerSyncWidgetState createState() => _ServerSyncWidgetState();
-}
-
-class _ServerSyncWidgetState extends State<ServerSyncWidget> {
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      Timer.periodic(const Duration(milliseconds: 100), (Timer t) => setState((){}));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(30),
-                splashColor: Colors.grey.withAlpha(50),
-                onTap: () {
-                  print("Hello World");
-                },
-                child: SizedBox(
-                  width: 370,
-                  height: 72 + _boxHeight,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(padding: const EdgeInsets.only(top: 16, left: 22), child: Align(alignment: Alignment.centerLeft, child: Text("Sync With Server", style: bodyStyle))),
-                      Container(padding: const EdgeInsets.only(top: 0, left: 22), child: Align(alignment: Alignment.centerLeft, child: Text("You have 3 unsaved changes.", style: bodyXSmallStyle))),
-                    ],
-                  ),
-                ),
-              ),
-              color: customColor,
-          );
-  }
-}
-
-class LiveStreamWidget extends StatefulWidget {
-  const LiveStreamWidget({Key? key}) : super (key: key);
-
-  @override
-  _LiveStreamWidgetState createState() => _LiveStreamWidgetState();
-}
-
-class _LiveStreamWidgetState extends State<LiveStreamWidget> {
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      Timer.periodic(const Duration(milliseconds: 100), (Timer t) => setState((){}));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(30),
-                splashColor: customColor.withAlpha(50),
-                onTap: () {
-                  print("Hello World");
-                },
-                child: SizedBox(
-                  width: 370,
-                  height: 72 + _boxHeight,
-                  child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    Container(padding: const EdgeInsets.only(left: 25, bottom: 5), child: Image.asset("assets/images/livestream_icon.png", fit: BoxFit.contain, height: 44)),
-                    Column(
-                      children: [
-                        Container(padding: const EdgeInsets.only(top: 16, left: 20), child: Align(alignment: Alignment.centerLeft, child: Text("Your event is live.", style: bodyStyle))),
-                        Container(padding: const EdgeInsets.only(top: 0, left: 0), child: Align(alignment: Alignment.centerLeft, child: Text("Click here to tune in.", style: bodyXSmallStyle))),
-                      ],
-                    ),
-                  ],)
-                ),
-              ),
-          );
-  }
-}
-
-class TeamRankingsWidget extends StatefulWidget {
-  const TeamRankingsWidget({Key? key}) : super (key: key);
-
-  @override
-  _TeamRankingsWidgetState createState() => _TeamRankingsWidgetState();
-}
-
-class _TeamRankingsWidgetState extends State<TeamRankingsWidget> {
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      Timer.periodic(const Duration(milliseconds: 100), (Timer t) => setState((){}));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(30),
-                splashColor: customColor.withAlpha(50),
-                onTap: () {
-                  print("Hello World");
-                },
-                child: SizedBox(
-                  width: 370,
-                  height: 400 + _boxHeight,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(padding: const EdgeInsets.only(top: 16, left: 22), child: Align(alignment: Alignment.centerLeft, child: Text("Team Rankings", style: bodyStyle))),
-
-                    ],
-                  ),
-                ),
-              ),
-          );
-  }
-}
-
 
 var _teamInformationWidget = const TeamInformationWidget();
 var _serverSyncWidget = const ServerSyncWidget();
@@ -347,7 +130,7 @@ class _DetectiveAppState extends State<DetectiveApp> {
               children: [
 
                 IconButton(
-                  icon: ServerState,
+                  icon: serverState,
                   // icon: Icon(Icons.link_off),
                   // icon: Icon(Icons.link),
                   onPressed: () => print("Settings"),
@@ -362,7 +145,7 @@ class _DetectiveAppState extends State<DetectiveApp> {
                 
                 Builder(
                   builder: (context) => IconButton(
-                    icon: Icon(Icons.settings),
+                    icon: const Icon(Icons.settings),
                     onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingsPage())),
                   ),
                 ),
@@ -446,10 +229,10 @@ class SettingsPage extends StatelessWidget {
                       width: 370,
                       child: TextField(
                         controller: TextEditingController(text: _SERVER_ADDRESS_),
-                        onSubmitted: (String value) => {_SERVER_ADDRESS_ = value},
+                        onChanged: (String value) => {__SERVER_ADDRESS_ = value},
                         maxLines: 1,
                         decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 15, top: 35,),
+                          contentPadding: const EdgeInsets.only(left: 15, top: 35,),
                           isDense: true,
                           filled: true,
                           fillColor: const Color(0xff424242),
@@ -463,7 +246,7 @@ class SettingsPage extends StatelessWidget {
                             borderRadius: BorderRadius.circular(30),
                           )
                         ),
-                        style: TextStyle(fontFamily: 'LeagueSpartan'),
+                        style: const TextStyle(fontFamily: 'LeagueSpartan'),
                         textAlignVertical: TextAlignVertical.center,
                       ),
                     ),
@@ -472,10 +255,10 @@ class SettingsPage extends StatelessWidget {
                       width: 370,
                       child: TextField(
                         controller: TextEditingController(text: _SERVER_PORT_),
-                        onSubmitted: (String value) => {_SERVER_PORT_ = value},
+                        onChanged: (String value) {__SERVER_PORT_ = value; print("value: $value");},
                         maxLines: 1,
                         decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 15, top: 35,),
+                          contentPadding: const EdgeInsets.only(left: 15, top: 35,),
                           isDense: true,
                           filled: true,
                           fillColor: const Color(0xff424242),
@@ -489,7 +272,7 @@ class SettingsPage extends StatelessWidget {
                             borderRadius: BorderRadius.circular(30),
                           )
                         ),
-                        style: TextStyle(fontFamily: 'LeagueSpartan'),
+                        style: const TextStyle(fontFamily: 'LeagueSpartan'),
                         textAlignVertical: TextAlignVertical.center,
                       ),
                     ),
@@ -498,11 +281,20 @@ class SettingsPage extends StatelessWidget {
                     ElevatedButton(
                       child: const Text("Save Changes and Exit."),
                       onPressed: () async {
-                        final directory = await getApplicationDocumentsDirectory();
-                        final AppFilePath = directory.path;
-                        print("APPFILEPATH $AppFilePath");
-                        final file = await File(AppFilePath + "/server.txt");
-                        file.writeAsString("$_SERVER_ADDRESS_\n$_SERVER_PORT_");
+                        // file.writeAsString("$_SERVER_ADDRESS_\n$_SERVER_PORT_");
+                        _SERVER_ADDRESS_ = __SERVER_ADDRESS_;
+                        _SERVER_PORT_ = __SERVER_PORT_;
+                        print("$_SERVER_ADDRESS_\n$_SERVER_PORT_");
+                        await writeFile("server.txt", "$_SERVER_ADDRESS_\n$_SERVER_PORT_");                        
+                        Navigator.pop(context);
+                      },
+                    ),
+                    
+                    ElevatedButton(
+                      child: const Text("CHECK FILE"),
+                      onPressed: () async {
+                        // file.writeAsString("$_SERVER_ADDRESS_\n$_SERVER_PORT_");
+                        print(await readFile("server.txt"));
                         Navigator.pop(context);
                       },
                     ),
---
+++ client/lib/styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+TextStyle headerStyle = const TextStyle(fontSize: 48, fontFamily: 'LeagueSpartan');
+TextStyle bodyStyle = const TextStyle(fontSize: 20, fontFamily: 'Roboto', color: Colors.white);
+TextStyle bodySmallStyle = const TextStyle(fontSize: 16, fontFamily: 'Roboto');
+TextStyle bodyXSmallStyle = const TextStyle(fontSize: 14, fontFamily: 'Roboto');
+TextStyle bodyItalSmallStyle = const TextStyle(fontSize: 14, fontFamily: 'Roboto', fontStyle: FontStyle.italic, color: Colors.white);
