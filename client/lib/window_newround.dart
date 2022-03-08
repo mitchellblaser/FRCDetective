c80c854a4353948bc2911ee54152bb2df8c5d21e
Mitch Blaser <mitchell.1080media@gmail.com>
2022-03-09 10:44:49 +1100

    Migrated Autonomous Widget to new Class/File

--- client/lib/window_newround.dart
+++ client/lib/window_newround.dart
@@ -6,6 +6,7 @@ import 'styles.dart';
 
 import 'widgets/newround/roundinfo.dart';
 import 'widgets/newround/notes.dart';
+import 'widgets/newround/autonomous.dart';
 
 class NewRoundInfo extends StatefulWidget {
   const NewRoundInfo({Key? key}) : super(key: key);
@@ -15,8 +16,6 @@ class NewRoundInfo extends StatefulWidget {
 }
 
 class _NewRoundInfoState extends State<NewRoundInfo> {
-
-  bool _didTaxi = false;
 
   @override
   void initState() {
@@ -61,87 +60,7 @@ class _NewRoundInfoState extends State<NewRoundInfo> {
                     
                     const RoundInfoWidget(),
                     const Padding(padding: EdgeInsets.only(top: 10)),
-
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(30),
-                          splashColor: customColor.withAlpha(50),
-                          child: SizedBox(
-                            width: 370,
-                            height: 200 + boxHeight,
-                            child: Column(
-                              children: [
-                                Container(padding: const EdgeInsets.only(top: 16, left: 22), child: Align(alignment: Alignment.centerLeft, child: Text("Autonomous", style: bodyStyle))),
-
-                                const Padding(padding: EdgeInsets.only(top: 20)),
-
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  children: [
-                                    const Padding(child: Text("High Goal"), padding: EdgeInsets.only(right: 50)),
-                                    Row(
-                                      children: [
-                                        ElevatedButton(
-                                          child: const Icon(Icons.keyboard_arrow_up),
-                                          onPressed: () {},
-                                        ),
-                                        Padding(child: Text("69", style: bodyStyle,), padding: const EdgeInsets.only(left: 10, right: 10)),
-                                        ElevatedButton(
-                                          child: const Icon(Icons.keyboard_arrow_down),
-                                          onPressed: () {},
-                                        ),
-                                      ],
-                                    )
-                                  ],
-                                ),
-
-                                const Padding(padding: EdgeInsets.only(top: 20)),
-
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  children: [
-                                    const Padding(child: Text("Low Goal"), padding: EdgeInsets.only(right: 50)),
-                                    Row(
-                                      children: [
-                                        ElevatedButton(
-                                          child: const Icon(Icons.keyboard_arrow_up),
-                                          onPressed: () {},
-                                        ),
-                                        Padding(child: Text("69", style: bodyStyle,), padding: const EdgeInsets.only(left: 10, right: 10)),
-                                        ElevatedButton(
-                                          child: const Icon(Icons.keyboard_arrow_down),
-                                          onPressed: () {},
-                                        ),
-                                      ],
-                                    )
-                                  ],
-                                ),
-
-                                const Padding(padding: EdgeInsets.only(top: 20)),
-
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Checkbox(
-                                      value: _didTaxi,
-                                      onChanged: (bool? value) {
-                                        setState(() {_didTaxi = value!;});
-                                      },
-                                    ),
-                                    const Padding(padding: EdgeInsets.only(left: 5)),
-                                    const Text("Crossed Taxi Line"),
-                                    const Padding(padding: EdgeInsets.only(right: 10))
-                                  ],
-                                )
-
-                              ],
-
-                            ),
-                          ),
-                        ),
-                    ),
+                    const AutonomousWidget(),
                     const Padding(padding: EdgeInsets.only(top: 10)),
 
                     Card(
