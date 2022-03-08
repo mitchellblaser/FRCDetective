882b119bc1df16eeab50d1d39e43eb512723a8a4
Mitch Blaser <mitchell.1080media@gmail.com>
2022-03-08 12:53:27 +1100

    Added notes section to round entry page

--- client/lib/window_newround.dart
+++ client/lib/window_newround.dart
@@ -102,25 +102,38 @@ class NewRoundInfo extends StatelessWidget {
                           ),
                         ),
                     ),
+                    const Padding(padding: EdgeInsets.only(top: 15)),
 
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(30),
-                          splashColor: customColor.withAlpha(50),
-                          child: SizedBox(
-                            width: 370,
-                            height: 170 + boxHeight,
-                            child: Column(
-                              children: [
-                                Container(padding: const EdgeInsets.only(top: 16, left: 22), child: Align(alignment: Alignment.centerLeft, child: Text("Notes", style: bodyStyle)))
-                              ],
+                    Container(
+                      width: 370,
+                      height: 205,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF424242),
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
 
+                      child: Column(children: [
+                        Container(padding: const EdgeInsets.only(top: 16, left: 22), child: Align(alignment: Alignment.centerLeft, child: Text("Notes", style: bodyStyle))),
+
+                        Container(
+                          padding: const EdgeInsets.only(left:15, right: 15, top: 8),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: const Color.fromARGB(255, 75, 75, 75),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)
+                              ),
                             ),
-                          ),
-                        ),
-                    )
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 5,
+                          )
+                        )
+
+                      ],)
+
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 15)),
 
 
                   ],)
