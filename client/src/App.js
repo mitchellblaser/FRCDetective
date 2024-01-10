1b6ef95c5635fb770f19c2aef39c41fbb97c3b11
Mitch Blaser <mitchell.1080media@gmail.com>
2024-01-10 23:43:06 +1100

    Think we are going to ditch this option...

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Link
+} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import logo from './logo.svg';
-import HelloWorld from './Components/HelloWorld';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Home from './Views/Home';
+import About from './Views/About';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <HelloWorld name="User"/>
+      <Router>
+        <Header/>
 
-      <Footer/>
+        <div className='p-3'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+          </Routes>
+        </div>
+
+        <Footer/>
+      </Router>
+
     </div>
   );
 }
---
+++ client/src/Views/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <div>
+            <h1 className='font-bold text-2xl'>Home Page</h1>
+        </div>
+    )
+}
+
+export default Home;
